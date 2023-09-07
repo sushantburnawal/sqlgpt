0e9e9504d04ce455a5370c5b5430e805091fc524
mansi <mansibansal2000@gmail.com>
2023-09-07 12:23:32 +0530

    streamlit added

--- sqlbase.py
+++ sqlbase.py
@@ -5,6 +5,7 @@ from langchain.llms.openai import OpenAI
 from langchain.agents import AgentExecutor 
 from langchain.agents.agent_types import AgentType
 from langchain.chat_models import ChatOpenAI
+import streamlit as st
 
 import environ
 env = environ.Env()
@@ -15,8 +16,6 @@ OPENAI_API_KEY = env('OPENAI_API_KEY')
 mssql_uri = f"mssql+pymssql://{env('DBUSER')}:{env('DBPASS')}@{env('DATABASE')}.database.windows.net:1433/{env('SERVER')}"
 
 db = SQLDatabase.from_uri(mssql_uri)
-
-OPENAI_API_KEY = "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"
 gpt = OpenAI(openai_api_key=OPENAI_API_KEY, model_name='gpt-3.5-turbo')
 
 toolkit = SQLDatabaseToolkit(db=db, llm=gpt)
@@ -27,5 +26,27 @@ agent_executor = create_sql_agent(
     agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
 )
 
-question = "Total number of users in the database"
-agent_executor.run(question)
+def ask_question(query):
+    
+    result = agent_executor.run(query)
+    return result["answer"]
+
+#command = input("User Question >> ")
+#response = ask_question(command)
+#print(f"\nAnswer : {response}\n")    
+
+
+st.title("QueryGPT 🧳 👨🏾‍⚖️")
+question_input = st.text_input("Ask a Question on the Database:")
+
+if question_input!="":
+    
+    with st.spinner("Searching. Please hold..."):
+
+        try:
+            st.write("Answer:\n\n",ask_question(question_input))
+        except Exception as e:
+            print(e)
+   
+        #st.write("Answer:\n\n",agent_executor.run(question_input))
+
